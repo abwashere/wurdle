003e2a96d4af3f594f9345cd41ec5f1a9053a00d
Audrey Belson <audrey.belson@unkle.fr>
2022-07-18 09:00:46 +0200

    clean css and start

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  // const rows = new Array(5)
+
+  // const resolveInput = () => {
+  //   return ["H","E","L","L","O"]
+  // }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Wordle</h1>
       </header>
+      <div className="App-viewer">
+        array of arrays letters
+      </div>
+      <div className="App-input">
+        input + button submit
+      </div>
     </div>
   );
 }
