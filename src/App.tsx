037b73babe4cc5983fdbe17b811776523f518efc
abwashere <audrey.belson@gmail.com>
2022-08-01 19:46:20 +0200

    fix tiles colors and grid length + finish logic of checking letters + format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,104 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 interface ILetter {
-  letter: string,
-  state: string,
+  letter: string;
+  state: string;
 }
 
-export const ANSWER = "MEMOS"
+export const ANSWER: string = "IMAMS";
 
-export const evaluateLetter = (letter: string, index: number) : ILetter => {
+export const checkAnswer = (lettersArr: string[]): ILetter[] => {
+  let statusArr = [];
 
-  for (let i=0; i < 5; i++) {
-    if (index === i && letter === ANSWER[i]) {
-      return {letter, state: "correct"}
+  // count each letter appearance in the answer
+  // only those which are present (but not at the right place)
+  let answerMap = new Map();
+  let ind = 0;
+  for (const letter of ANSWER.split("")) {
+    if (letter !== lettersArr[ind]) {
+      answerMap.set(letter, (answerMap.get(letter) || 0) + 1);
+    }
+    ind++;
+  }
+
+  for (let i = 0; i < 5; i++) {
+    if (lettersArr[i] === ANSWER[i]) {
+      statusArr.push({ letter: lettersArr[i], state: "correct" });
+    } else if (
+      answerMap.has(lettersArr[i]) &&
+      answerMap.get(lettersArr[i]) > 0
+    ) {
+      answerMap.set(lettersArr[i], answerMap.get(lettersArr[i]) - 1);
+      statusArr.push({ letter: lettersArr[i], state: "present" });
+    } else {
+      statusArr.push({ letter: lettersArr[i], state: "absent" });
     }
   }
 
-  return { letter, state: "absent" }
-}
+  return statusArr;
+};
 
-export const Letter = (props: ILetter ) => {
+export const Letter = (props: ILetter) => {
   const { letter, state } = props;
-  return <div className={`letter ${state}`}>{letter}</div>
-}
+  return <div className={`letter ${state}`}>{letter}</div>;
+};
 
 export const Row = ({ attempt }: { attempt: string }) => {
-  const lettersArr = attempt === "" ? ["","","","",""] : attempt.split("")
-  const evaluatedLetters = lettersArr.map((letter, i) => evaluateLetter(letter, i))
+  if (attempt === "") {
+    return (
+      <div className="word">
+        {["", "", "", "", ""].map((el, i) => {
+          return <Letter key={i} letter={""} state={""} />;
+        })}
+      </div>
+    );
+  }
 
-  const word = <div className="word">
-    {evaluatedLetters.map((el, i) => {
-      const { letter, state } = el;
-      return <Letter key={i} letter={letter} state={state} />
-    })}
-  </div>
+  const lettersArr = attempt.split("");
+  const evaluatedLetters = checkAnswer(lettersArr);
+  // const evaluatedLetters = lettersArr.map((letter, i) => evaluateLetter(letter, i))
 
-  return word
-}
+  return (
+    <div className="word">
+      {evaluatedLetters.map((el, i) => {
+        const { letter, state } = el;
+        return <Letter key={i} letter={letter} state={state} />;
+      })}
+    </div>
+  );
+};
 
 function App() {
-  const [guess, setGuess] = React.useState("")
+  const [guess, setGuess] = React.useState("");
+  const [attemptList, setAttemptList] = React.useState(["", "", "", "", "", ""]);
 
-  const [attemptList, setAttemptList] = React.useState(["", "", "", "", ""])
-
-  console.log(attemptList)
-  
   const handleChange = (e: any) => {
-    let val = e.target.value
-    setGuess(guess => val.toUpperCase())
-  }
-  
+    let val = e.target.value;
+    setGuess(val.toUpperCase());
+  };
+
   const handleSubmit = (e: any) => {
-    e.preventDefault()
-    
-    for (let i=0; i<=6; i++) {
-      if (attemptList[i] === ""){
-        setAttemptList(attemptList => {
-          const newList = [...attemptList]
+    e.preventDefault();
+
+    for (let i = 0; i <= 6; i++) {
+      if (attemptList[i] === "") {
+        setAttemptList((attemptList) => {
+          const newList = [...attemptList];
           newList[i] = guess;
-          return newList
-        })
+          return newList;
+        });
         break;
       }
     }
-  }
+
+    setGuess("");
+  };
+
+  const isDisabled =
+    guess.length !== 5 ||
+    guess.includes(" ") ||
+    attemptList.every((el) => el !== "");
 
   return (
     <div className="App">
@@ -79,8 +114,15 @@ function App() {
 
       <div className="App-form">
         <form onSubmit={handleSubmit}>
-          <input type="text" onChange={handleChange} maxLength={5} /> 
-          <button onSubmit={handleSubmit} disabled={guess.length !== 5}>submit</button>
+          <input
+            type="text"
+            onChange={handleChange}
+            value={guess}
+            maxLength={5}
+          />
+          <button onSubmit={handleSubmit} disabled={isDisabled}>
+            submit
+          </button>
         </form>
       </div>
     </div>
